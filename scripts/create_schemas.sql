975f24c0b8024d5835e2f653d3da5e36b512e5b8
Youssef Cherkaoui <youssef.cherkaoui1234@gmail.com>
2025-12-17 21:59:24 -0500

    added the create_schemas script

---
+++ scripts/create_schemas.sql
@@ -0,0 +1,62 @@
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info (
+	cst_id INT PRIMARY KEY,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date NVARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT PRIMARY KEY,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt Datetime,
+    prd_end_dt Datetime
+);
+
+
+
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num NVARCHAR(50) PRIMARY KEY,
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
+);
+
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+	CID NVARCHAR(50) PRIMARY KEY,
+    BDATE Date,
+    GEN NVARCHAR(50)
+);
+
+
+
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+	CID NVARCHAR(50) PRIMARY KEY,
+    CNTRY NVARCHAR(50)
+);
+
+
+
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+	ID NVARCHAR(50) PRIMARY KEY,
+    CAT NVARCHAR(50),
+    SUBCAT NVARCHAR(50),
+    MAINTENANCE NVARCHAR(50)
+);
+
