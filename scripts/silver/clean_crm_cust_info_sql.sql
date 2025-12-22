bffe289a3007aed3cd458a52da7c87423c19e2ff
Youssef Cherkaoui <youssef.cherkaoui1234@gmail.com>
2025-12-21 19:48:22 -0500

    deleted the gender added in cust info table

--- scripts/silver/clean_crm_cust_info_sql.sql
+++ scripts/silver/clean_crm_cust_info_sql.sql
@@ -12,24 +12,11 @@ WHERE cst_id = '';
 DROP TABLE IF EXISTS noblank_table;
 CREATE TEMPORARY TABLE noblank_table as
 	SELECT cst_id, MAX(cst_key) as cst_key, MAX(cst_firstname) as cst_firstname, MAX(cst_lastname) as cst_lastname,
-	MAX(cst_marital_status) as cst_marital_status, MAX(cst_gndr) as cst_gndr, MIN(cst_create_date) as cst_create_date
+	MAX(cst_marital_status) as cst_marital_status, cst_gndr, MIN(cst_create_date) as cst_create_date
 	FROM trimmed_table
 	GROUP BY cst_id
 	;
-
-DROP TABLE IF EXISTS filtered_names_table;
-CREATE TEMPORARY TABLE filtered_names_table as
-	with filter_names_by_gender_cte as
-	(
-		SELECT *,
-		ROW_NUMBER() OVER(PARTITION BY `name` ORDER BY total_count DESC) as row_num
-		FROM names_info.cust_name_table as cust_names
-	)
-	SELECT `name`, gender
-	FROM filter_names_by_gender_cte
-	where row_num = 1
-	;
-    
+   
 DROP TABLE IF EXISTS cleaned_crm_cust_info_table;
 CREATE TEMPORARY TABLE cleaned_crm_cust_info_table as
 	SELECT noblank_table.cst_id, noblank_table.cst_key, noblank_table.cst_firstname, noblank_table.cst_lastname,
@@ -37,14 +24,9 @@ case
 		WHEN noblank_table.cst_marital_status = 'S' then 'Single'
         WHEN noblank_table.cst_marital_status = 'M' then 'Married'
     END as cst_marital_status,
-case 
-		when cust_names.gender = 'M' then 'Male'
-        when cust_names.gender = 'F' then 'Female'
-    END as cust_gndr,    
+	noblank_table.cst_gndr, 
     noblank_table.cst_create_date
 	FROM noblank_table
-	JOIN filtered_names_table as cust_names
-	ON cust_names.`name` = noblank_table.cst_firstname
 	ORDER BY noblank_table.cst_id
 	;
 
